c7877bd11c482e8f23c997cb0ed296c50847f41e
Andrii Pavliuk <devdice@outlook.com>
2020-07-17 00:09:47 +0300

    Changed regex to field

--- Backend.Walker/Infrastructure/Fireflow.HtmlParser/HtmlParser.cs
+++ Backend.Walker/Infrastructure/Fireflow.HtmlParser/HtmlParser.cs
@@ -7,12 +7,11 @@ namespace Fireflow.HtmlParser
 {
     public class HtmlParser : IHtmlParser
     {
+        private readonly Regex regex = new Regex("<img.+?src=[\"'](.+?)[\"'].+?>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
         public IList<string> GetImageSourceUrls(string source, string ext)
         {
-            var pattern = "<img.+?src=[\"'](.+?)[\"'].+?>";
-            var options = RegexOptions.IgnoreCase | RegexOptions.Multiline;
-
-            var matchedImageSources = Regex.Matches(source, pattern, options);
+            var matchedImageSources = regex.Matches(source);
 
             return (from Match m in matchedImageSources select m.Groups[1].Value).ToList();
         }
