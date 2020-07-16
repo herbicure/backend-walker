542b7bfe480ea0f0a48048e24a89ea70e0700845
Andrii Pavliuk <devdice@outlook.com>
2020-07-17 00:00:32 +0300

    Updated

--- Backend.Walker/Fireflow.Security.Owin.Instagram/InstagramAuthenticationHandler.cs
+++ Backend.Walker/Fireflow.Security.Owin.Instagram/InstagramAuthenticationHandler.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Owin.Security;
+﻿using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Fireflow.Security.Owin.Instagram
 {
